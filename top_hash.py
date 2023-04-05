85cf0c982c544fcb728be82ca28781436abfd5f5
charityCyberSecurity <112363458+charityCyberSecurity@users.noreply.github.com>
2023-04-05 12:16:24 -0400

    Initial Commit

---
+++ top_hash.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import glob, os, hashlib
+
+os.getcwd()
+
+hashValue = [] 
+#hashValue 
+for file in os.listdir("files"):
+    if file.endswith(".txt"):
+        with open(os.path.join("files", file), 'rb') as f:
+            print(file)
+            file_contents = f.read()
+            hash=hashlib.sha1(file_contents).hexdigest()
+            hashValue.append(hash)
+
+print("Hash value of all the contnts:")
+print(hashValue)
+print("\n")
+if(len(hashValue)%2!=0):
+        hashValue.append(hashValue[-1])
+while(len(hashValue)>1):
+    j=0
+    for i in range(0, len(hashValue)-1):
+        f = str(hashValue[i]+hashValue[i+1])
+        hashValue[j]=hashlib.sha1(f.encode()).hexdigest()
+        
+        i+=2
+        j+=1
+    del hashValue[j:]
+
+if (len(hashValue)==1):
+    print("Top Hash is:") 
+    print(hashValue)
+    with open("check.aap", "a") as a:
+        a.write("yess")
+    print("The Top Hash has been saved in check.aap") 
+        
+    
